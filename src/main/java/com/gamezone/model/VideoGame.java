7d4d02375c31dad0dca7d7bfaf3a8f121e717d68
Camilo Andrés Maestre Dávila <camaestredavila@unicesar.edu.co>
2026-09-06 15:58:37 -0500

    feat: implement VideoGame class extending Product

--- src/main/java/com/gamezone/model/VideoGame.java
+++ src/main/java/com/gamezone/model/VideoGame.java
@@ -1,4 +1,54 @@
 package com.gamezone.model;
 
-public class VideoGame {
+public class VideoGame extends Product{
+
+    private String platform;
+    private String genre;
+    private String classification;
+
+    public VideoGame(String id, String title, double price, int stockQuantity, String platform, String genre, String classification) {
+        super(id, title, price, stockQuantity);
+        this.platform = platform;
+        this.genre = genre;
+        this.classification = classification;
+    }
+
+    public String getPlatform() {
+        return platform;
+    }
+
+    public void setPlatform(String platform) {
+        this.platform = platform;
+    }
+
+    public String getGenre() {
+        return genre;
+    }
+
+    public void setGenre(String genre) {
+        this.genre = genre;
+    }
+
+    public String getClassification() {
+        return classification;
+    }
+
+    public void setClassification(String classification) {
+        this.classification = classification;
+    }
+
+    @Override
+    public String getDescription() {
+        return "VideoGame{" +
+                "id='" + getId() + '\'' +
+                ", title='" + getTitle() + '\'' +
+                ", price=" + getPrice() +
+                ", stockQuantity=" + getStockQuantity() +
+                ", platform='" + platform + '\'' +
+                ", genre='" + genre + '\'' +
+                ", classification='" + classification + '\'' +
+                '}';
+    }
+
+
 }
