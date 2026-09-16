548816bb592f7faf641673ec35fc19320e793e43
Camilo Andrés Maestre Dávila <camaestredavila@unicesar.edu.co>
2026-09-16 15:47:27 -0500

    feat: implement BulkPurchaseDiscount subclass

---
+++ src/main/java/com/gamezone/model/BulkPurchaseDiscount.java
@@ -0,0 +1,41 @@
+package com.gamezone.model;
+
+import java.time.LocalDate;
+
+public class BulkPurchaseDiscount extends Promotion{
+
+    private int minimumQuantity;
+    private double discountPercentage;
+
+    public BulkPurchaseDiscount(String id, String name, LocalDate startDate, LocalDate endDate, int minimumQuantity, double discountPercentage) {
+        super(id, name, startDate, endDate);
+        this.minimumQuantity = minimumQuantity;
+        this.discountPercentage = discountPercentage;
+    }
+
+    public int getMinimumQuantity() {
+        return minimumQuantity;
+    }
+
+    public void setMinimumQuantity(int minimumQuantity) {
+        this.minimumQuantity = minimumQuantity;
+    }
+
+    public double getDiscountPercentage() {
+        return discountPercentage;
+    }
+
+    public void setDiscountPercentage(double discountPercentage) {
+        this.discountPercentage = discountPercentage;
+    }
+
+    @Override
+    public double calculateDiscount(Sale sale) {
+        int totalQuantity = sale.getProducts().size();
+        if (totalQuantity >= minimumQuantity) {
+            double total = sale.calculateTotal();
+            return total * (discountPercentage / 100);
+        }
+        return 0;
+    }
+}
