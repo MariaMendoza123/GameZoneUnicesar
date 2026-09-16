038c6cf02a3d22b9d18746f9c4b1f98346b36145
Camilo Andrés Maestre Dávila <camaestredavila@unicesar.edu.co>
2026-09-16 12:21:08 -0500

    feat: add abstract base class Promotion

---
+++ src/main/java/com/gamezone/model/Promotion.java
@@ -0,0 +1,57 @@
+package com.gamezone.model;
+
+import java.time.LocalDate;
+
+public abstract class Promotion {
+
+    private String id;
+    private String name;
+    private LocalDate startDate;
+    private LocalDate endDate;
+
+    public Promotion(String id, String name, LocalDate startDate, LocalDate endDate) {
+        this.id = id;
+        this.name = name;
+        this.startDate = startDate;
+        this.endDate = endDate;
+    }
+
+
+    public String getId() {
+        return id;
+    }
+
+    public void setId(String id) {
+        this.id = id;
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+
+    public LocalDate getStartDate() {
+        return startDate;
+    }
+
+    public void setStartDate(LocalDate startDate) {
+        this.startDate = startDate;
+    }
+
+    public LocalDate getEndDate() {
+        return endDate;
+    }
+
+    public void setEndDate(LocalDate endDate) {
+        this.endDate = endDate;
+    }
+
+    public boolean isActive(LocalDate date) {
+        return (date.isEqual(startDate) || date.isAfter(startDate)) && (date.isEqual(endDate) || date.isBefore(endDate));
+    }
+
+    public abstract double calculateDiscount(Sale sale);
+}
